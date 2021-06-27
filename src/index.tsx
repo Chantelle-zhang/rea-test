8ea604dc87be585d1dcd25fedaf3af33f8438c6d
chantelle <maudlin.nk@gmail.com>
2021-06-27 13:40:12 +1000

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+import store from "./store";
+
+
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+
