827bdf0aa25cbd8ee9ed251c3d6fe31d5f955d25
chantelle <maudlin.nk@gmail.com>
2021-06-27 12:49:20 +1000

    create rootReducer

---
+++ src/store/slices/index.ts
@@ -0,0 +1,11 @@
+
+import { combineReducers } from 'redux'
+import propertyListReducer from './propertyList'
+
+
+const rootReducer = combineReducers({
+  propertyList: propertyListReducer,
+
+})
+
+export default rootReducer
